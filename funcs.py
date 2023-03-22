97159f3470c6f536cdb60200b784d3fec6ac9024
RRusansh2005 <128546386+RRusansh2005@users.noreply.github.com>
2023-03-23 02:11:39 +0300

    2.3.1

--- funcs.py
+++ funcs.py
@@ -15,6 +15,7 @@ class Rules(StatesGroup):
     request = State()
 
 
+
 def access_try(user_id: int) -> int:
     """На будущее, когда необходимо будет контролировать доступ к боту"""
     return 1 if user_id in ACCESS else 0
