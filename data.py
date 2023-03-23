d7521029ee56f7e299b66134770cae9b7f579394
Pearson77 <mialal0505@gmail.com>
2023-03-23 20:19:51 +0300

    4.2

--- data.py
+++ data.py
@@ -2,35 +2,37 @@ import sqlite3
 
 db = sqlite3.connect("database.db")
 cursor = db.cursor()
-cursor.execute("""CREATE TABLE inf_answers (
-'1' TEXT,
-'2' TEXT,
-'3' TEXT,
-'4' TEXT,
-'5' TEXT,
-'6' TEXT,
-'7' TEXT,
-'8' TEXT,
-'9' TEXT,
-'10' TEXT,
-'11' TEXT,
-'12' TEXT,
-'13' TEXT,
-'14' TEXT,
-'15' TEXT,
-'16' TEXT,
-'17' TEXT,
-'18' TEXT,
-'19' TEXT,
-'20' TEXT,
-'21' TEXT,
-'22' TEXT,
-'23' TEXT,
-'24' TEXT,
-'25' TEXT,
-'26' TEXT,
-'27' TEXT
-)""")
+array = cursor.execute(f"SELECT n2 FROM inf_answers WHERE rowid > '0'").fetchall()
+print(array)
+# cursor.execute("""CREATE TABLE inf_answers (
+# '1' TEXT,
+# '2' TEXT,
+# '3' TEXT,
+# '4' TEXT,
+# '5' TEXT,
+# '6' TEXT,
+# '7' TEXT,
+# '8' TEXT,
+# '9' TEXT,
+# '10' TEXT,
+# '11' TEXT,
+# '12' TEXT,
+# '13' TEXT,
+# '14' TEXT,
+# '15' TEXT,
+# '16' TEXT,
+# '17' TEXT,
+# '18' TEXT,
+# '19' TEXT,
+# '20' TEXT,
+# '21' TEXT,
+# '22' TEXT,
+# '23' TEXT,
+# '24' TEXT,
+# '25' TEXT,
+# '26' TEXT,
+# '27' TEXT
+# )""")
 db.commit()
 db.close()
 
